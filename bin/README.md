994b4e042340f7df0cdc6e5e9f45d67502de9bf3
John Hannah <john@friendlymachine.net>
2017-04-06 17:43:28 -0400

    basic set up

---
+++ bin/README.md
@@ -0,0 +1,1 @@
+Add the BrowserStackLocal binary for your system to this folder. You can find the binaries here: [https://www.browserstack.com/local-testing](https://www.browserstack.com/local-testing).
---
+++ browserStackLocal.js
@@ -0,0 +1,17 @@
+import { exec } from 'child_process';
+require('dotenv').config();
+const cmd = `${__dirname}/bin/BrowserStackLocal --key ${process.env.BROWSERSTACK_AUTOMATE_KEY}`;
+
+console.init();
+
+exec(cmd, (error, stdout, stderr) => {
+  if (error) {
+    console.error('Error starting BrowserStack Local', error);
+    return;
+  }
+  console.log('stdout:', stdout);
+  console.error('stderr:', stderr);
+  return;
+});
+
+console.log('BrowserStackLocal started...');
